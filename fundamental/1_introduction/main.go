76948441ef464309616bb7f9781b9feed8138fd6
unthenamed <unthenamed@gmail.com>
2024-10-01 00:24:50 +0700

    Removing worng mkdir

--- fundamental/1_introduction/main.go
+++ fundamental/1_introduction/main.go
@@ -1,11 +1,3 @@
-// ///////////////////////////////
-//
-//	Chapter 1					//
-//	Golang Introduction			//
-//	Abdul Jalil khoironi		//
-//
-// ///////////////////////////////
-
 package main
 
 import (
